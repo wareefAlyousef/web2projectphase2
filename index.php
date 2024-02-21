05f11b6054038a26ca8e10fc9e7225e6b72b1ac7
BashairAlsadhan <bashair.sadhan@gmail.com>
2024-02-21 17:37:48 +0300

    cc

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-asdsdffgygtyuio
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+
+    <?php
+        echo "hi";
+       
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
